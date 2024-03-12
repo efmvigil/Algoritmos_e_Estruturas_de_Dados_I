73ac37ddadfc5be2a2f7f69dace0b637b7721b9b
Eduardo Vigil <efmvigil@gmail.com>
2024-03-11 21:39:40 -0300

    Aula 02 - métodos construtores

---
+++ Aula 02/main.py
@@ -0,0 +1,10 @@
+from Pessoa import Pessoa
+
+p1 = Pessoa("Maria", 25)
+print("Nome: ", p1.nome)
+
+p2 = Pessoa(idade=20, nome="João")
+print("Idade: ", p2.idade)
+
+p3 = Pessoa("Carlos")
+print("Idade: ", p2.idade)
